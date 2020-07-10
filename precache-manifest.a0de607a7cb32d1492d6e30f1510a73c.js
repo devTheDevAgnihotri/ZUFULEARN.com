2ff121546c5912afc5faa70177b67b26c4d52f47
devagni98 <44253776+devagni98@users.noreply.github.com>
2020-07-10 11:59:25 +0530

    Add files via upload

---
+++ precache-manifest.a0de607a7cb32d1492d6e30f1510a73c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31861fc6c24f6022cd10024e22df589f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "64343773fd762124faa5",
+    "url": "/static/css/main.b12958cf.chunk.css"
+  },
+  {
+    "revision": "69d5a318a7f5352e47f6",
+    "url": "/static/js/2.b539f66b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b539f66b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64343773fd762124faa5",
+    "url": "/static/js/main.6681e5d5.chunk.js"
+  },
+  {
+    "revision": "eba6a59a1fa9fbdc1159",
+    "url": "/static/js/runtime-main.6a04bd64.js"
+  }
+]);
